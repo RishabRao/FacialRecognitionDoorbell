476f08eb35a32bda25b3771c4aa5902f7b9609c3
RishabRao <rishabpaws@gmail.com>
2019-05-02 21:13:47 -0400

    cleaned up and changed some variable names

--- RingtoneBell/record_faces.py
+++ RingtoneBell/record_faces.py
@@ -23,21 +23,21 @@ def record_faces():
     cursor.execute('INSERT INTO users (name) VALUES (?)', (name,))
     user_id = cursor.lastrowid
 
-    sample_image = 0
+    sample_image_count = 0
 
     while True:
         ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
-            sample_image = sample_image + 1
-            cv2.imwrite("dataset/User." + str(user_id) + "." + str(sample_image) + ".jpg", gray[y:y + h, x:x + w])
+            sample_image_count = sample_image_count + 1
+            cv2.imwrite("dataset/User." + str(user_id) + "." + str(sample_image_count) + ".jpg", gray[y:y + h, x:x + w])
             cv2.putText(img, "Recording face", (500, 660), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv2.waitKey(100)
         cv2.imshow('img', img)
         cv2.waitKey(1)
-        if sample_image > 20:
+        if sample_image_count > 20:
             break
     cap.release()
     database.commit()
